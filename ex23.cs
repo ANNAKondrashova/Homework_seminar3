19a7796b822ec4225a3753dceaaeacddce4fa5b2
ANNAKondrashova <117078208+ANNAKondrashova@users.noreply.github.com>
2022-11-15 12:52:33 +0300

    Add files via upload

---
+++ ex23.cs
@@ -0,0 +1,4 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int num = SetNumber("N");
+int[] arr = CubeTable(num);
+System.Console.WriteLine(String.Join(",", arr));
