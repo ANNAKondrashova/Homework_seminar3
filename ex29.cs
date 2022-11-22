bebb5032a9ce64d8e3161d1427486865cff359a3
ANNAKondrashova <117078208+ANNAKondrashova@users.noreply.github.com>
2022-11-22 20:30:42 +0300

    Add files via upload

---
+++ ex29.cs
@@ -0,0 +1,19 @@
+﻿void ViewArray()
+{
+    Console.Clear();
+    int size = SetNumber("size");
+    int minValue = SetNumber("minValue");
+    int maxValue = SetNumber("maxValue");
+    int [] array = GetRandomArray(size, minValue, maxValue);
+    Console.WriteLine($"[{String.Join(",", array)}]");
+}
+
+int [] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result [i] = new Random().Next(minValue, maxValue);
+    }
+    return result;
+}
