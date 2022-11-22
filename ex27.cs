77881ca4a6d421185fbd5ac2f22926c040c23b1c
ANNAKondrashova <117078208+ANNAKondrashova@users.noreply.github.com>
2022-11-22 20:20:15 +0300

    Add files via upload

---
+++ ex27.cs
@@ -0,0 +1,17 @@
+﻿void SumNumbers()
+{
+    Console.Clear();
+    int num = SetNumber("N");
+    Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+}
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
